0a7ab1d22960e9137e2ee2b0da03ba993c38a594
g-song-i <57793091+g-song-i@users.noreply.github.com>
2023-12-29 02:38:41 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double avg;
+    cin >> avg;
+
+    if (avg >= 1.0) {
+        cout << "High";
+    } else if (avg >= 0.5) {
+        cout << "Middle";
+    } else {
+        cout << "Low";
+    }
+
+    return 0;
+}
