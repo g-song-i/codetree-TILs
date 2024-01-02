9f2bd9a8a9aa7547b084f04606432f3fabfc0251
g-song-i <57793091+g-song-i@users.noreply.github.com>
2024-01-02 16:51:53 +0900

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240102/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -5,17 +5,16 @@ int main() {
     int a_math, a_eng, b_math, b_eng;
     cin >> a_math >> a_eng >> b_math >> b_eng;
 
-    if (a_math >= b_math) {
-        cout << "A";
-    } else if (b_math == a_math) {
+    if (a_math == b_math) {
         if (a_eng >= b_eng) {
             cout << "A";
         } else {
             cout << "B";
         }
+    } else if (a_math > b_math) {
+        cout << "A";
     } else {
         cout << "B";
     }
-    
     return 0;
 }
