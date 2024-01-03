1efa3255b3b2bc2b0eb7234b2ab692f0b6cb51e6
g-song-i <57793091+g-song-i@users.noreply.github.com>
2024-01-04 01:05:39 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240104/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i;
+    cin >> a >> b;
+    i = a;
+
+    while (i <= b) {
+        cout << i << " ";
+        i += 2;
+    }
+
+    return 0;
+}
