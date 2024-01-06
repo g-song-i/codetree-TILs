9161cb6e91fde979ebebd1d7e6ca6838345b0248
g-song-i <57793091+g-song-i@users.noreply.github.com>
2024-01-06 13:11:28 +0900

    Title: 최소공배수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240106/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void FindLcm(int n, int m) {
+    int lcm;
+    lcm = 0;
+
+    for (int i = 1; ;) {
+        
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
