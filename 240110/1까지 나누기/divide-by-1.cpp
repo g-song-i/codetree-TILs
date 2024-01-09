24f3a673277c395ffe772f8f5b6a6ebc2768de74
g-song-i <57793091+g-song-i@users.noreply.github.com>
2024-01-10 02:06:50 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int cnt, i;
+    cnt = 0;
+    i = 1;
+
+    while (n > 1) {
+        n /= i;
+        cnt++;
+        i++;
+
+        if (n == 0) {
+            cout << cnt;
+            break;
+        }
+    }
+    return 0;
+}
