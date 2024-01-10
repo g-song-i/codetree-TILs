460d042aa7e1c3ee5e63e834a7d96afd9e263a55
g-song-i <57793091+g-song-i@users.noreply.github.com>
2024-01-10 09:19:52 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240110/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool satisfied = false;
+
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0) {
+            satisfied = true;
+        }
+    }
+
+    if (satisfied) {
+        cout << 'C';
+    } else {
+        cout << 'N';
+    }
+    return 0;
+}
