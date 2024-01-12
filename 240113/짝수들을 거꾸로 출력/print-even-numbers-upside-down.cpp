69a0c972b27927aca1f334ccadea0a96e389cca6
g-song-i <57793091+g-song-i@users.noreply.github.com>
2024-01-13 01:01:09 +0900

    Title: 짝수들을 거꾸로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240113/짝수들을 거꾸로 출력/print-even-numbers-upside-down.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (arr[i] % 2 == 0) {
+            cout << arr[i] << " ";
+        } else {
+            continue;
+        }
+    }
+    return 0;
+}
