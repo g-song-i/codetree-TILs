9439ceb93fa9c13b9feb98113120872aeadbc536
g-song-i <57793091+g-song-i@users.noreply.github.com>
2024-01-12 17:37:04 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240112/시험 통과 여부 확인하기 2/verify-test-passed-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int studentNum;
+    int score[studentNum];
+    int cnt = 0;
+
+    cin >> studentNum;
+
+    for (int i = 0; i < studentNum; i++) {
+        int sum = 0;
+        double avg = 0;
+
+        for (int j = 0; j < 4; j++) {
+            cin >> score[i];
+            sum += score[i];
+        }
+
+        avg = sum / (double) 4;
+
+        if (avg >= 60) {
+            cout << "pass" << endl;
+            cnt++;
+        } else {
+            cout << "fail" << endl;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
